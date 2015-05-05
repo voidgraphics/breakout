1ee89ee665f95d2c4f948ebfa96dc1b6b2d4c94b
Adrien Leloup <nittiyh@outlook.com>
2015-05-05 19:39:05 +0200

    Add check brick hitzones function

--- js/breakout.js
+++ js/breakout.js
@@ -19,18 +19,17 @@
 		};
 
 		// Bricks
-		var Brick = function( x, y ) {
-			var iWidth, iHeight, iX, iY;
-			this.iWidth = 30;
-			this.iHeight = 8;
-			this.iX = x;
-			this.iY = y;
+		var Brick = function( iX, iY ) {
+			this.width = 30;
+			this.height = 8;
+			this.x = iX;
+			this.y = iY;
 		};
 
 		Brick.prototype.render = function() {
 			oApplication.context.fillStyle = "crimson";
 			aBricks.forEach( function( element ) {
-				oApplication.context.fillRect( element.iX, element.iY, element.iWidth, element.iHeight );
+				oApplication.context.fillRect( element.x, element.y, element.width, element.height );
 			} );
 		};
 
@@ -69,10 +68,10 @@
 		var oProjectile = {
 			"color": "#00CC66",
 			"size": 15,
-			"speed": 5,
+			"speed": 3,
 			"posX": oApplication.canvas.width / 2 - 15 / 2,
 			"posY": oApplication.canvas.height - 45,
-			"angle": -45,
+			"angle": 45,
 			"update": function() {
 				// Movement
 				var newX, newY;
@@ -109,7 +108,7 @@
 				}
 
 				// Hitting top side of platform
-				if( this.posY + this.size >= oPlatform.posY && this.posX > oPlatform.posX - this.size && this.posX + this.size <= oPlatform.posX + oPlatform.width ){
+				if( this.posY + this.size >= oPlatform.posY && this.posX > oPlatform.posX - this.size && this.posX + this.size <= oPlatform.posX + oPlatform.width  ){
 					if( this.angle == 135 ){
 						this.angle = 45;
 					} else if( this.angle == -135 ){
@@ -117,6 +116,7 @@
 					}
 				}
 
+				fCheckBricksHitzones();
 
 				// Calculate trajectory based on direction angle (ranging from -180 to 180 degrees)
 				if ( this.angle >= 90){
@@ -180,7 +180,7 @@
 				console.log( "Created a brick!" );
 
 				if( i == topBrickCount ){
-					iXOffset =  iPaddingX + lineNumber * ( ( aBricks[0].iWidth / 2 ) + ( Brick.spacing - aBricks[0].iWidth ) / 2 );
+					iXOffset =  iPaddingX + lineNumber * ( ( aBricks[0].width / 2 ) + ( Brick.spacing - aBricks[0].width ) / 2 );
 					iYOffset += 25;
 					lineNumber++;
 					topBrickCount--;
@@ -192,6 +192,23 @@
 				}
 			}
 			console.log("Done! Ready to start...");
+		};
+
+		var fCheckBricksHitzones = function() {
+			aBricks.forEach( function( element, index, array ){
+				if( oProjectile.posY <= element.y + element.height && oProjectile.posX >= element.x && oProjectile.posX + oProjectile.size >= element.x && oProjectile.posX + oProjectile.size <= element.x + element.width && oProjectile.posY > element.y ){
+					// Projectile top hits brick's bottom
+					aBricks.splice(aBricks.indexOf(element), 1);
+				} else if ( oProjectile.posY + oProjectile.size >= element.y && oProjectile.posX >= element.x  && oProjectile.posX + oProjectile.size >= element.x && oProjectile.posX + oProjectile.size <= element.x + element.width && oProjectile.posY <= element.y + element.height ){
+					// Projectile's bottom hits brick's top
+					aBricks.splice(aBricks.indexOf(element), 1);
+				} else if ( ((oProjectile.posY >= element.y + element.height && oProjectile.posY + oProjectile.size >= element.y + element.height && oProjectile.posY < element.y + element.height ) 
+							|| ( oProjectile.posY <= element.y && oProjectile.posY + oProjectile.size <= element.y + element.height && oProjectile.posY + oProjectile.size > element.y ) ) 
+							&& ( oProjectile.posX <= element.x + element.width && oProjectile.posX >= element.x )  ) {
+					// Projectile's left hits brick's right
+					aBricks.splice(aBricks.indexOf(element), 1);
+				}
+			} );
 		};
 
 		var fShowStartscreen = function() {
