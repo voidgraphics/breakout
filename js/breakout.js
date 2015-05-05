ea478679940ec3761807f6d2944efdce9766b376
Adrien Leloup <nittiyh@outlook.com>
2015-05-05 09:51:03 +0200

    First commit!

---
+++ js/breakout.js
@@ -0,0 +1,115 @@
+// Code principal
+( function(){
+	"use strict";
+
+	window.Breakout = function( oApplication ){
+
+		// Global params
+		var iAnimationRequestId = 0,
+			aBricks = [];
+
+		// Background
+		var oBackground = {
+			"color": "black",
+			"render": function() {
+				oApplication.context.fillStyle = this.color;
+				oApplication.context.fillRect( 0, 0, oApplication.width, oApplication.height );
+			}
+		};
+
+		// Bricks
+		var Brick = function() {
+			var iWidth, iHeight;
+			iWidth = 25;
+			iHeight = 5;
+		};
+
+		Brick.prototype.render = function() {
+			oApplication.context.fillStyle = "crimson";
+
+		};
+
+		Brick.prototype.update = function() {
+			this.render();
+		};
+
+		var oPlatform = {
+			"width": 100,
+			"height": 10,
+			"init": function(){},
+			"update": function(){},
+			"render": function(){}
+		};
+
+
+		// start game
+		var init = function() {
+			console.log( "Initialising game" );
+			// Draw background
+			oBackground.render();
+			// Generate bricks
+			fGenerateBricks();
+			// Draw platform
+			oPlatform.init();
+
+		};
+
+		var start = function() {
+			console.log( "Starting game!" );
+			oApplication.canvas.removeEventListener( "click", start );
+			fAnimationLoop();
+		};
+
+		var fGenerateBricks = function() {
+			console.log( "Generating bricks!" );
+			for( var i = 0; i < 50 ; i++ ){
+				aBricks.push( new Brick );
+				console.log( "Created a brick!" );
+			}
+		};
+
+		// Called at each animation frame request
+		var fAnimationLoop = function() {
+			iAnimationRequestId = window.requestAnimationFrame( fAnimationLoop );
+			// clear canvas
+			oApplication.context.clearRect( 0, 0, oApplication.width, oApplication.height );
+			// draw background
+			oBackground.render();
+			// update & draw bricks
+			for( var i=0; i < aBricks.length; i+=2 ){
+				aBricks[ i ].update();
+			}
+			// update platform
+			oPlatform.update();
+
+			oPlatform.render();
+		};
+
+		// Game over
+		var fGameOver = function() {
+			window.cancelAnimationFrame( iAnimationRequestId );
+			window.alert( "Perdu !" );
+			// Relancer le jeu
+			window.location.reload( true );
+		};
+
+		// init game: Loads the spritesheet, then launches a function when it's done loading.
+		window.addEventListener( "load", init );
+		oApplication.canvas.addEventListener( "click", start );
+
+
+		// Modifier l'écart entre les tubes
+		window.addEventListener( "keypress", function( e ){
+			if( e.keyCode == 38 ){
+				// Up key
+				console.log( "up" );
+
+			} else if( e.keyCode == 40 ) {
+				// Down key
+				console.log( "down" );
+			}
+		} );
+
+	};
+
+} )();
---
+++ js/game.js
@@ -0,0 +1,33 @@
+// Initialise le jeu
+( function(){
+	"use strict";
+
+	var app = {
+		"canvas": null,
+		"context": null,
+		"width": null,
+		"height": null
+	};
+
+	var _isCanvasSupported = function( $canvasElt ){
+		return !!$canvasElt.getContext;
+	};
+
+	app.setup = function(){
+		this.canvas = document.querySelector( "#game" );
+
+		if( !_isCanvasSupported( this.canvas ) ){
+			return console.error( "Canvas isn't supported!" );
+		}
+
+		this.context = this.canvas.getContext( "2d" );
+		this.width = this.canvas.width;
+		this.height = this.canvas.height;
+
+		var game = new window.Breakout( this ); // window.Breakout se trouve dans breakout.js
+
+	};
+
+	app.setup();
+
+} )();
